a7fb5541e2dd1c37868fa8030ac16bb1c9a4471b
Joanna <18332426+jbstks@users.noreply.github.com>
2018-06-09 14:35:03 -0300

    Updated comments
    in ValidatorTest.java for fuller understanding

--- app/src/test/java/com/csci3130/jbstks/password_validator/ValidatorTest.java
+++ app/src/test/java/com/csci3130/jbstks/password_validator/ValidatorTest.java
@@ -16,26 +16,32 @@ public class ValidatorTest {
     @Test
     public void testIsNotPassword() {
         // Should fail "is not 'password'" rule; pass "is at least 8 characters" rule
+        // is "password"
         pw = "password";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is not 'password'" rule; pass "is at least 8 characters" rule
+        // is "password" with uppercase letter
         pw = "Password";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is not 'password'" rule; pass "is at least 8 characters" rule
+        // is "password" with uppercase letters
         pw = "paSSword";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should pass "is not 'password'" rule; fail "is at least 8 characters" rule
+        // is not "password"
         pw = "test123";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should pass both rules
+        // contains "password" but is not strictly "password"
         pw = "password123";
         assertEquals(2, passwordTest.validate(pw));
 
         // Should pass both rules
+        // is not "password"
         pw = "test1234";
         assertEquals(2, passwordTest.validate(pw));
     }
@@ -43,38 +49,47 @@ public class ValidatorTest {
     @Test
     public void testIsAtLeast8Char() {
         // Should pass "is at least 8 characters" rule; fail "is not 'password'" rule
+        // 8 character password
         pw = "password";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should pass both tests
+        // 9 character password
         pw = "test12345";
         assertEquals(2, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 1 character password
         pw = "1";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 2 character password
         pw = "12";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 3 character password
         pw = "123";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 4 character password
         pw = "1234";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 5 character password
         pw = "12345";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 6 character password
         pw = "123456";
         assertEquals(1, passwordTest.validate(pw));
 
         // Should fail "is at least 8 characters" rule; pass "is not 'password'" rule
+        // 8 character password
         pw = "1234567";
         assertEquals(1, passwordTest.validate(pw));
     }
