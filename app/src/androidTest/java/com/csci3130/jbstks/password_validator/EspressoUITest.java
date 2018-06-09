9174a6db9b7f9fe784cb9095fa568031f53a69ec
Joanna <18332426+jbstks@users.noreply.github.com>
2018-06-09 16:24:22 -0300

    Started UI and Testing

---
+++ app/src/androidTest/java/com/csci3130/jbstks/password_validator/EspressoUITest.java
@@ -0,0 +1,28 @@
+package com.csci3130.jbstks.password_validator;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
---
+++ app/src/main/java/com/csci3130/jbstks/password_validator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.csci3130.jbstks.password_validator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * @author Joanna Bistekos (B00710704)
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Function to be called when the VALIDATE button is pressed by the user;
+     * Updates textView with the text inputted by the user in the text field
+     *
+     * @param view the view object that was clicked
+     */
+    public void validate(View view) {
+        Validator validator = new Validator();
+        EditText pwField = findViewById(R.id.pwField);
+        String pw = pwField.getText().toString();
+
+        TextView textView = findViewById(R.id.textView);
+        if (validator.validate(pw) == 5) textView.setText("Success!");
+        else textView.setText("Not strong enough!");
+        textView.setVisibility(View.VISIBLE);
+    }
+}
